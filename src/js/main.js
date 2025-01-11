13749f2ae7f85221380140ce8be1de6f6f4f6ac6
María García Antúnez <maria.garcia.antunez@gmail.com>
2025-01-11 14:36:18 +0100

    Create function themes with colours

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+
+// Previsualización de tarjeta
+
+const themes = {
+    theme1: {
+        nameColor: "#264653",
+        jobColor: "#2a9d8f",
+        iconsColor: "#e9c46a",
+    },
+    theme2: {
+        nameColor: "#f4a261",
+        jobColor: "#e76f51",
+        iconsColor: "#d62828",
+    },
+    theme3: {
+        nameColor: "#8ecae6",
+        jobColor: "#219ebc",
+        iconsColor: "#023047",
+    },
+};
+
