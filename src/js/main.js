f3784a4f83c0280084abd2e48f374c9f55dceb18
martaxxa <mchacartegui29@gmail.com>
2025-01-11 11:05:14 +0100

    Add first try

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('>> Ready :)');
+const btnImage = document.querySelector('.js_btnimage');
+
+// Evento para botón imagen
+btnImage.addEventListener('change', (event) => {
+    event.preventDefault();
+    const file = event.target.files[0];
+
+    if(file){
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            previewImage.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    } else{
+        previewImage.src = '';
+    }
+});
+
+console.log(reader);
