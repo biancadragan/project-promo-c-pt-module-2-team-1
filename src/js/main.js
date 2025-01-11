202536932edf911d008d2f52d0a3112efe765e68
Celia Recio <celia.recio@yahoo.es>
2025-01-11 14:59:39 +0100

    start share button

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+//selectores de contenido
+const createBtn = document.querySelector('.js_createBtn'); 
+
+//contenido dinámico
+
+
+
+
+//funciones para botón 
+const shareTwitter = (ev) => {
+
+} 
+
+createBtn.addEventListener('click'(ev)) => {
+
+}
