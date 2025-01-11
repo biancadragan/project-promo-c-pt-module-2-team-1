fec53958167d16742487f7dd67f2b13f2f70bee1
martaxxa <mchacartegui29@gmail.com>
2025-01-11 12:10:59 +0100

    Add input tipo file

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,2 @@
 'use strict';
 
-const btnImage = document.querySelector('.js_btnimage');
-
-// Evento para botón imagen
-btnImage.addEventListener('change', (event) => {
-    event.preventDefault();
-    const file = event.target.files[0];
-
-    if(file){
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            previewImage.src = e.target.result;
-        };
-        reader.readAsDataURL(file);
-    } else{
-        previewImage.src = '';
-    }
-});
-
-console.log(reader);
