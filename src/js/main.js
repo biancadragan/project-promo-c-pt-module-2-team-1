32ac0f9f9e587273571ceeacc3178c0ed228f4bf
Celia Recio <celia.recio@yahoo.es>
2025-01-14 22:05:16 +0100

    switch to main to copy data objects

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,16 @@ console.log('>> Ready :)');
 
 //selectores de contenido
 const createBtn = document.querySelector('.js_createBtn'); 
+const shareBtn = document.querySelector('js_shareBtn'); 
+const data {
+    
+}
 
 //evento y funciones
 function handleClickCreate = (ev) => {
     ev.preventDefault(); 
-    console.log (data)
+    console.log (data); 
+
 }; 
 
 createBtn.addEventListener('click', handleClickCreate); 
