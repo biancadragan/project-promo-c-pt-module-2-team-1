675b32e0441ccd4edb994b117cb05966bda7e3ca
Celia Recio <celia.recio@yahoo.es>
2025-01-14 22:08:28 +0100

    copy rest of JS from main

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,17 @@ console.log('>> Ready :)');
 //selectores de contenido
 const createBtn = document.querySelector('.js_createBtn'); 
 const shareBtn = document.querySelector('js_shareBtn'); 
-const data {
-    
-}
+//objeto de datos
+const data = {
+    palette: 1,
+    name: '',
+    job: '',
+    photo: '',
+    phone: '',
+    email: '',
+    linkedin: '',
+    github: ''
+};
 
 //evento y funciones
 function handleClickCreate = (ev) => {
