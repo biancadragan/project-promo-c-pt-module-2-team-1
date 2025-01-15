1be1a5d1ad9448c1ff7f2798f8229c83ba04cc1b
Celia Recio <celia.recio@yahoo.es>
2025-01-15 23:07:06 +0100

    add name to target and change to API objects

--- src/js/main.js
+++ src/js/main.js
@@ -38,7 +38,7 @@ const applyTheme = (theme) => {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const selectedTheme = document.querySelector('input[name="theme"]:checked').value;
+    const selectedTheme = document.querySelector('input[name="field1"]:checked').value;
     applyTheme(themes[selectedTheme]); // Aplica el tema basado en el radio seleccionado
 });
 
@@ -60,13 +60,13 @@ import './get-avatar.js';
 // PREVIEW Section
 const data = {
     palette: 1,
-    name: '',
-    job: '',
+    field2: '',
+    field3: '',
     photo: '',
-    phone: '',
-    email: '',
-    linkedin: '',
-    github: ''
+    field4: '',
+    field5: '',
+    field6: '',
+    field7: ''
 };
 
 // Selección de elementos del formulario y previsualización
@@ -80,7 +80,7 @@ const githubPreview = document.querySelector('.js_preview_github');
 
 // Manejador del input
 const handleInput = (ev) => {
-    const field = ev.target.id; // ID del campo del formulario
+    const field = ev.target.name; // ID del campo del formulario
     data[field] = ev.target.value; // Actualiza el objeto `data`
 
 
@@ -91,20 +91,20 @@ const handleInput = (ev) => {
 // Función para actualizar la previsualización
 function updatePreview() {
     // Actualizamos nombre y puesto
-    namePreview.innerHTML = (data.name === '') ? 'Nombre Apellido' : data.name;
-    jobPreview.innerHTML = (data.job === '') ? 'Posición' : data.job;
+    namePreview.innerHTML = (data.field2 === '') ? 'Nombre Apellido' : data.field2;
+    jobPreview.innerHTML = (data.field3 === '') ? 'Posición' : data.field3;
 
     // Actualizamos enlaces de contacto
-    phonePreview.setAttribute('href', data.phone ? `tel:${data.phone}` : '');
-    emailPreview.setAttribute('href', data.email ? `mailto:${data.email}` : '');
+    phonePreview.setAttribute('href', data.field5 ? `tel:${data.field5}` : '');
+    emailPreview.setAttribute('href', data.field4 ? `mailto:${data.field4}` : '');
 
     // Ocultamos íconos si los campos están vacíos
-    phonePreview.style.display = data.phone ? 'inline-block' : 'none';
-    emailPreview.style.display = data.email ? 'inline-block' : 'none';
-    linkedInPreview.setAttribute('href', data.linkedin ? data.linkedin : '');
-    linkedInPreview.style.display = data.linkedin ? 'inline-block' : 'none';
-    githubPreview.setAttribute('href', data.github ? data.github : '');
-    githubPreview.style.display = data.github ? 'inline-block' : 'none';
+    phonePreview.style.display = data.field5 ? 'inline-block' : 'none';
+    emailPreview.style.display = data.field4 ? 'inline-block' : 'none';
+    linkedInPreview.setAttribute('href', data.field6 ? data.field6 : '');
+    linkedInPreview.style.display = data.field6 ? 'inline-block' : 'none';
+    githubPreview.setAttribute('href', data.field7 ? data.field7 : '');
+    githubPreview.style.display = data.field7 ? 'inline-block' : 'none';
 }
 
 // Escuchamos el evento `input` en el formulario
@@ -124,8 +124,6 @@ const shareBtn = document.querySelector('.js_shareBtn');
 
 function handleClickCreate (ev) {
     ev.preventDefault(); 
-
-    const dataToSend {}
 
     console.log(data);
 
