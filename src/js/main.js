ec6f96b6d701a792c32ac4d8e0d723300e78537a
María García Antúnez <maria.garcia.antunez@gmail.com>
2025-01-17 00:28:28 +0100

    Fix URL Bluesky

--- src/js/main.js
+++ src/js/main.js
@@ -5,60 +5,54 @@ console.log(">> Ready :)");
 // Previsualización de tarjeta
 
 const themes = {
-  theme1: {
-    nameColor: "#264653",
-    jobColor: "#2a9d8f",
-    iconsColor: "#e9c46a",
-  },
-  theme2: {
-    nameColor: "#f4a261",
-    jobColor: "#e76f51",
-    iconsColor: "#d62828",
-  },
-  theme3: {
-    nameColor: "#8ecae6",
-    jobColor: "#219ebc",
-    iconsColor: "#023047",
-  },
+    theme1: {
+        nameColor: "#264653",
+        jobColor: "#2a9d8f",
+        iconsColor: "#e9c46a",
+    },
+    theme2: {
+        nameColor: "#f4a261",
+        jobColor: "#e76f51",
+        iconsColor: "#d62828",
+    },
+    theme3: {
+        nameColor: "#8ecae6",
+        jobColor: "#219ebc",
+        iconsColor: "#023047",
+    },
 };
 
-<<<<<<< HEAD
 const previewName = document.querySelector(".js_preview_name");
 const previewJob = document.querySelector(".js_preview_job");
 const previewIcons = document.querySelectorAll(".js_previewIcon");
-=======
-const previewName = document.querySelector('.js_preview_name');
-const previewJob = document.querySelector('.js_preview_job');
-const previewIcons = document.querySelectorAll('.js_previewIcon');
->>>>>>> 6409d06376fd7a02f661586d856ce782917b19d7
 
 const applyTheme = (theme) => {
-  previewName.style.color = theme.nameColor;
-  previewJob.style.color = theme.jobColor;
-  previewIcons.forEach((icon) => {
-    icon.style.color = theme.iconsColor;
-  });
+    previewName.style.color = theme.nameColor;
+    previewJob.style.color = theme.jobColor;
+    previewIcons.forEach((icon) => {
+        icon.style.color = theme.iconsColor;
+    });
 };
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  const selectedTheme = document.querySelector(
-    'input[name="field1"]:checked'
-  ).value;
-  applyTheme(themes[selectedTheme]); // Aplica el tema basado en el radio seleccionado
+    const selectedTheme = document.querySelector(
+        'input[name="field1"]:checked'
+    ).value;
+    applyTheme(themes[selectedTheme]); // Aplica el tema basado en el radio seleccionado
 });
 
 // Event listeners para cada radio button
 document.getElementById("palette1").addEventListener("change", () => {
-  applyTheme(themes.theme1); // Aplica el tema 1
+    applyTheme(themes.theme1); // Aplica el tema 1
 });
 
 document.getElementById("palette2").addEventListener("change", () => {
-  applyTheme(themes.theme2); // Aplica el tema 2
+    applyTheme(themes.theme2); // Aplica el tema 2
 });
 
 document.getElementById("palette3").addEventListener("change", () => {
-  applyTheme(themes.theme3); // Aplica el tema 3
+    applyTheme(themes.theme3); // Aplica el tema 3
 });
 
 import "./get-avatar.js";
@@ -66,14 +60,14 @@ import "./get-avatar.js";
 // PREVIEW Section
 
 let data = {
-  field1: "theme1",
-  field2: "",
-  field3: "",
-  photo: "",
-  field4: "",
-  field5: "",
-  field6: "",
-  field7: "",
+    field1: "theme1",
+    field2: "",
+    field3: "",
+    photo: "",
+    field4: "",
+    field5: "",
+    field6: "",
+    field7: "",
 };
 
 // Selección de elementos del formulario y previsualización
@@ -87,37 +81,31 @@ const githubPreview = document.querySelector(".js_preview_github");
 
 // Manejador del input
 const handleInput = (ev) => {
-  const field = ev.target.name; // ID del campo del formulario
-  data[field] = ev.target.value; // Actualiza el objeto `data`
+    const field = ev.target.name; // ID del campo del formulario
+    data[field] = ev.target.value; // Actualiza el objeto `data`
 
-  // Llama a la función para actualizar la previsualización
-  updatePreview();
+    // Llama a la función para actualizar la previsualización
+    updatePreview();
 };
 
 // Función para actualizar la previsualización
 function updatePreview() {
-<<<<<<< HEAD
-  // Actualizamos nombre y puesto
-  namePreview.innerHTML = data.field2 === "" ? "Nombre Apellido" : data.field2;
-  jobPreview.innerHTML = data.field3 === "" ? "Posición" : data.field3;
-=======
 
     // Actualizamos nombre y puesto
     namePreview.innerHTML = data.field2 === "" ? "Nombre Apellido" : data.field2;
     jobPreview.innerHTML = data.field3 === "" ? "Posición" : data.field3;
->>>>>>> 6409d06376fd7a02f661586d856ce782917b19d7
 
-  // Actualizamos enlaces de contacto
-  phonePreview.setAttribute("href", data.field5 ? `tel:${data.field5}` : "");
-  emailPreview.setAttribute("href", data.field4 ? `mailto:${data.field4}` : "");
+    // Actualizamos enlaces de contacto
+    phonePreview.setAttribute("href", data.field5 ? `tel:${data.field5}` : "");
+    emailPreview.setAttribute("href", data.field4 ? `mailto:${data.field4}` : "");
 
-  // Ocultamos íconos si los campos están vacíos
-  phonePreview.style.display = data.field5 ? "inline-block" : "none";
-  emailPreview.style.display = data.field4 ? "inline-block" : "none";
-  linkedInPreview.setAttribute("href", data.field6 ? data.field6 : "");
-  linkedInPreview.style.display = data.field6 ? "inline-block" : "none";
-  githubPreview.setAttribute("href", data.field7 ? data.field7 : "");
-  githubPreview.style.display = data.field7 ? "inline-block" : "none";
+    // Ocultamos íconos si los campos están vacíos
+    phonePreview.style.display = data.field5 ? "inline-block" : "none";
+    emailPreview.style.display = data.field4 ? "inline-block" : "none";
+    linkedInPreview.setAttribute("href", data.field6 ? data.field6 : "");
+    linkedInPreview.style.display = data.field6 ? "inline-block" : "none";
+    githubPreview.setAttribute("href", data.field7 ? data.field7 : "");
+    githubPreview.style.display = data.field7 ? "inline-block" : "none";
 }
 
 // Escuchamos el evento `input` en el formulario
@@ -136,51 +124,59 @@ const cardResultSection = document.querySelector(".js__card_result");
 //Acciones de botón "Crear tarjeta"
 
 function handleClickCreate(ev) {
-  ev.preventDefault();
+    ev.preventDefault();
 
-  console.log(data);
+    console.log(data);
 
-  //fetch con peticiones al servidor y promesas
+    //fetch con peticiones al servidor y promesas
 
-  data.field1 = parseInt(data.field1.replace("theme", ""));
-  fetch("https://dev.adalab.es/api/info/data", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  }) //enviar datos
-    .then((response) => response.json())
-    .then((responseData) => {
-      console.log(responseData);
+    data.field1 = parseInt(data.field1.replace("theme", ""));
+    fetch("https://dev.adalab.es/api/info/data", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    }) //enviar datos
+        .then((response) => response.json())
+        .then((responseData) => {
+            console.log(responseData);
 
-      //resultados y botón de envío de la tarjeta a Bluesky
-      if (responseData.success === true) {
-        cardResultSection.innerHTML = `
+            //resultados y botón de envío de la tarjeta a Bluesky
+            if (responseData.success === true) {
+
+                const infoID = responseData.infoID;
+                const baseUrl = `https://dev.adalab.es/api/info/${infoID}`;
+                const text = encodeURIComponent("¡Mira mi tarjeta de visita diseñada en ProCard Creator!");
+
+                const blueskyUrl = `https://bsky.app/intent/compose?text=${text}&url=${encodeURIComponent(baseUrl)}`;
+
+
+                cardResultSection.innerHTML = `
         <h2 class="share_result__title">¡Aquí tienes el enlace a tu ProCard!</h2>
         <p class="share_result__subtitle">Que la disfrutes &#x1F604</p>
-        <a class="share_result__link" href="https://dev.adalab.es/api/info/${responseData.infoID}">https://dev.adalab.es/api/info/${responseData.infoID}</a>
-        <a class="share_result__shareButton" href="https://bsky.app/intent/compose?text=TU_TEXTO_AQUÍ&url=TU_URL_AQUÍ${responseData.infoID}">Compartir en Bluesky</a>
+        <a class="share_result__link" href="${baseUrl}">${baseUrl}</a>
+        <a class="share_result__shareButton" href="${blueskyUrl}" target="_blank">Compartir en Bluesky</a>
         `; //pinta un texto de confirmación y un enlace a Bluesky
 
-      } else {
-        const errorEnCastellano = responseData.error
-          .replace("Mandatory fields", "Campos obligatorios")
-          .replace("field2", "nombre")
-          .replace("field3", "puesto")
-          .replace("field4", "email")
-          .replace("field5", "teléfono")
-          .replace("field6", "LinkedIn")
-          .replace("field7", "Github")
-          .replace("photo", "foto");
-        cardResultSection.innerHTML = `
+            } else {
+                const errorEnCastellano = responseData.error
+                    .replace("Mandatory fields", "Campos obligatorios")
+                    .replace("field2", "nombre")
+                    .replace("field3", "puesto")
+                    .replace("field4", "email")
+                    .replace("field5", "teléfono")
+                    .replace("field6", "LinkedIn")
+                    .replace("field7", "Github")
+                    .replace("photo", "foto");
+                cardResultSection.innerHTML = `
         <p>Desafortunadamente no se ha podido guardar la información</p>
         <p>${errorEnCastellano}</p>
         `; //genera un mensaje de error si no se rellenan los campos necesarios para generar la tarjeta
-      }
-    })
+            }
+        })
 
-    .catch((error) => {
-      console.error("Error al enviar los datos:", error);
-    });
+        .catch((error) => {
+            console.error("Error al enviar los datos:", error);
+        });
 }
 
 //evento para que al hacer click ejecute la acción
@@ -196,31 +192,31 @@ const profileImageReset = document.querySelector(".js__profile-image");
 const profilePreviewReset = document.querySelector(".js__profile-preview");
 
 let clearData = (ev) => {
-  console.log("funciono");
+    console.log("funciono");
 
-  data = {
-    field1: "theme1",
-    field2: "",
-    field3: "",
-    photo: "",
-    field4: "",
-    field5: "",
-    field6: "",
-    field7: "",
-  };
+    data = {
+        field1: "theme1",
+        field2: "",
+        field3: "",
+        photo: "",
+        field4: "",
+        field5: "",
+        field6: "",
+        field7: "",
+    };
 
-  profileImageReset.style.backgroundImage = "url('../images/Profile-pic.png')";
-  profilePreviewReset.style.backgroundImage =
-    "url('../images/Profile-pic.png')";
+    profileImageReset.style.backgroundImage = "url('../images/Profile-pic.png')";
+    profilePreviewReset.style.backgroundImage =
+        "url('../images/Profile-pic.png')";
 
-  // Limpiar el formulario
-  form.reset();
+    // Limpiar el formulario
+    form.reset();
 
-  // Limpiar la previsualización
-  updatePreview(data);
+    // Limpiar la previsualización
+    updatePreview(data);
 
-  // Restablecer el tema por defecto
-  applyTheme(themes.theme1);
+    // Restablecer el tema por defecto
+    applyTheme(themes.theme1);
 };
 
 btnDelete.addEventListener('click', clearData);
